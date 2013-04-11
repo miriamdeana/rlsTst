09a73d756386fb207eda8b1f236dbb875714ed83
Miriam DeAna <miriam.williams@careerbuilder.com>
2013-04-11 15:14:31 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RlsTst::Application.config.secret_token = 'ccdf22c6b29f925e0bb20380e7e1c2871c22a58c5a5d23d4c236439abb1c55bb3b568a5ec09cf5b6866e5a4904ef74fc539eec9608d5055976cbeb520280c7bc'
